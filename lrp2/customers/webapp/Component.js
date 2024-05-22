cd7c81477e82fd55f2486557ea5b74d48f1c5169
Rafael Gomez de Lama <rafael.gomez@t4sadvance.com>
2024-05-22 15:34:15 +0000

    Commit inicial

---
+++ lrp2/customers/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("customers.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
